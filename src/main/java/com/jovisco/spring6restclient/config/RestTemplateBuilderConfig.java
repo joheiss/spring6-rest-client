e04d0bd3fa4841424ccd99dfd733bb6fc6da35d2
Josef Heiss <josef.heiss@jovisco.de>
2024-06-01 13:12:12 +0200

    section 44 - zalando logbook

--- src/main/java/com/jovisco/spring6restclient/config/RestTemplateBuilderConfig.java
+++ src/main/java/com/jovisco/spring6restclient/config/RestTemplateBuilderConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class RestTemplateBuilderConfig {
@@ -48,8 +50,13 @@ public class RestTemplateBuilderConfig {
 
         assert baseUrl != null;
 
+        var logbookInterceptor = new LogbookClientHttpRequestInterceptor(Logbook.builder().build());
+
         return configurer.configure(new RestTemplateBuilder())
-                .additionalInterceptors(interceptor)
+                .additionalInterceptors(
+                    interceptor, 
+                    logbookInterceptor
+                )
                 .uriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
     }
 }
